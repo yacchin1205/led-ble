1efe1fdaa31eb5e24bdb12f8b5138e1b51e0d6e0
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2024-06-09 14:48:27 +0900

    Fix FastLED logic

--- arduino/led-ble/sinks.cpp
+++ arduino/led-ble/sinks.cpp
@@ -22,8 +22,11 @@ void AnalogWriteChannelSink::apply(int frame) {
 
 FastLEDChannelManager::FastLEDChannelManager(int num_leds) {
     this->num_leds = num_leds;
+    this->rgbs = (rgb_t*)malloc(sizeof(rgb_t) * num_leds);
+    memset(this->rgbs, 0, sizeof(rgb_t) * num_leds);
     this->leds = (CRGB*)malloc(sizeof(CRGB) * num_leds);
     memset(this->leds, 0, sizeof(CRGB) * num_leds);
+    this->dirty = false;
 }
 
 FastLEDChannelManager::~FastLEDChannelManager() {
@@ -31,15 +34,49 @@ FastLEDChannelManager::~FastLEDChannelManager() {
         free(this->leds);
         this->leds = NULL;
     }
+    if (this->rgbs != NULL) {
+        free(this->rgbs);
+        this->rgbs = NULL;
+    }
 }
 
 CRGB* FastLEDChannelManager::getLeds() {
     return this->leds;
 }
 
+void FastLEDChannelManager::setLeds(int led, int channel, float value) {
+    rgb_t* rgb = &this->rgbs[led];
+    if (channel == 0) {
+      if (rgb->r == value) {
+        return;
+      }
+      rgb->r = value;
+    } else if (channel == 1) {
+      if (rgb->g == value) {
+        return;
+      }
+      rgb->g = value;
+    } else if (channel == 2) {
+      if (rgb->b == value) {
+        return;
+      }
+      rgb->b = value;
+    }
+    this->dirty = true;
+}
+
+
 void FastLEDChannelManager::onApply(int frame) {
+    if (!this->dirty) {
+        return;
+    }
     // Serial.printf("FastLEDChannelManager::onApply for #%d\n", frame);
+    for (int i = 0; i < this->num_leds; i ++) {
+        rgb_t* rgb = &this->rgbs[i];
+        this->leds[i].setRGB((int8_t)rgb->r, (int8_t)rgb->g, (int8_t)rgb->b);
+    }
     FastLED.show();
+    this->dirty = false;
 }
 
 FastLEDChannelSink::FastLEDChannelSink(uint8_t sink, FastLEDChannelManager* manager, int led, int channel): ChannelSink(sink) {
@@ -52,8 +89,8 @@ FastLEDChannelSink::~FastLEDChannelSink() {
 }
 
 void FastLEDChannelSink::setValue(int frame, float value) {
-    // Serial.printf("FastLEDChannel(%d)::setValue(%f) for #%d\n", this->led, value, frame);
-    this->manager->getLeds()[this->led][this->channel] = (int)value;
+    // Serial.printf("FastLEDChannel(%d)::Channel(%d)::setValue(%f) for #%d\n", this->led, this->channel, value, frame);
+    this->manager->setLeds(this->led, this->channel, value);
 }
 
 void FastLEDChannelSink::apply(int frame) {
